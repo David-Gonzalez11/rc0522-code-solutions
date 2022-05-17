0fd9ab472a99597468a7dad7981732a780176a10
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-17 17:39:26 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,67 @@
+var orderHistory = [
+  {
+    dateDelivered: 'Aug 8, 2020',
+    orderNumber: 114 - 3941689 - 8772232,
+    total: 34.00,
+    orderPlaced: 'Aug 4, 2020',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'closed on Sep 7, 2020',
+        priceofItem: 31.55
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    shipTo: 'JS Masher',
+    orderNumber: 113 - 9984268 - 1280257,
+    dateDelivered: 'Jul 20, 2020',
+    item: [
+      {
+        title: 'The Timeless way of building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'closed on Aug 10, 2020',
+        priceofItem: 41.33
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    shipTo: 'JS Masher',
+    orderNumber: 114 - 2875557 - 9059409,
+    dateDelivered: 'Jul 7, 2020',
+    item: [
+      {
+        type: 'hardware',
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube adapter for WII U, PC, Support turbo and vibration features.',
+        returnWindow: 'closed on Aug 5, 2020',
+        priceofItem: 15.98
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    shipTo: 'JS Masher',
+    orderNumber: 113 - 28831777 - 2648248,
+    dateDelivered: 'Jul 5, 2020',
+    item: [
+      {
+        type: 'hardware',
+        title: 'GameCube Controller- Super Smash Bros. Edition(Nintento Switch)',
+        returnWindow: 'closed on Aug 4, 2020',
+        priceofItem: 94.95
+      },
+      {
+        type: 'book',
+        returnUntil: 'Aug 4, 2020',
+        name: 'The Art of Sql',
+        priceOfItem: 33.99,
+        returnWindow: 'closed on Aug 4, 2020'
+      }
+    ]
+
+  }
+];
