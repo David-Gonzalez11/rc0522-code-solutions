6a1fdba8e4075330be1e22d5fe994a3436c59209
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-19 19:59:11 +0000

    Added pseudocode to half the problems

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,4 +1,8 @@
 /* exported initial */
+// create a storage for all elements that are not the lat element
+// iterate through all the elements in the array
+// push the element at index of i to the storage
+// return the storage which contains all elemnts without the last
 function initial(array) {
   var newArr = [];
   for (var i = 0; i < array.length - 1; i++) {
