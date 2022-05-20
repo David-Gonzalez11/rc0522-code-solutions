409e6bae62aa5fb035cd6bdd939701a311deaad6
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-20 00:47:02 +0000

    Made changes to variable name

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -2,6 +2,6 @@
 // create a new variable and set its values to them object at its key value
 // return the object variable
 function getValue(object, key) {
-  var Object = object[key];
-  return Object;
+  var objects = object[key];
+  return objects;
 }
