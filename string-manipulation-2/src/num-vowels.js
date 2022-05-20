76fd3c0d867a7ef8a4a7c27f5671c5013dff1af1
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-20 02:14:44 +0000

    Completed string-manipulation-2

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,9 +1,16 @@
 /* exported numVowels */
 function numVowels(string) {
+  // create a new string to make all the letters in string lowercase
+  // create a new variable called count and set it equal to 0
+  // iterate over the string
+  // check if the newString variable at i is equal to a vowel
+  // if it is then increment the count variable by 1
+  // return the count variable
+  var newString = string.toLowerCase();
   var count = 0;
   for (var i = 0; i < string.length; i++) {
-    if (string.charAt(i) === 'a' || string.charAt(i) === 'A' || string.charAt(i) === 'e' || string.charAt(i) === 'E' || string.charAt(i) === 'i' ||
-string.charAt(i) === 'o' || string.charAt(i) === 'u') {
+    if (newString.charAt(i) === 'a' || newString.charAt(i) === 'e' || newString.charAt(i) === 'i' ||
+newString.charAt(i) === 'o' || newString.charAt(i) === 'u') {
       count++;
     }
   }
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,4 +1,16 @@
 /* exported swapChars */
+/*
+create a new varibale and set it equal to an empty string
+create another variable and set it equal to the firstIndex of the string
+create another variable and set it equal to the secondIndex of the string
+iterate through the string
+check if index at i is equal to the firstIndex
+if it is then chars is equal to the second variable
+check  if i is equal to the second index
+if it is then chars is equal to first variable
+if anythign else then chars is equal to the string at i index
+return chars variable
+*/
 function swapChars(firstIndex, secondIndex, string) {
   var chars = '';
   var first = string[firstIndex];
