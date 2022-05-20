ec698704edc862d8f0a04a7963acdedb892d4457
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-20 00:42:56 +0000

    Made changes to variable name

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -2,6 +2,6 @@
 // create  a variable and set its value to the object at key and then assign that value to the value parameter
 // return the object variable just created
 function setValue(object, key, value) {
-  var Object = (object[key] = value);
-  return Object;
+  var objects = (object[key] = value);
+  return objects;
 }
