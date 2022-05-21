7b2e2bd9f33c984c8d35a1fb0f096a2fa94bf893
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-21 22:01:14 +0000

    Need to do titleCsse algorithm

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,4 +1,9 @@
 /* exported isPalindromic */
+
+// create a new variable to replace all spaces with no spaces
+// check if words variable is equal to the reversed string
+// if it is then return true
+// if not return false
 function isPalindromic(string) {
   var words = string.replaceAll(' ', '');
   if (words === words.split('').reverse().join('')) {
