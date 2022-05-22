7c55d5234c15339ef4366427913d02986f8dcbdc
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-22 04:46:11 +0000

    In progress with zip array

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,12 @@
 /* exported zip */
+function zip(arr1, arr2) {
+  const pairArr = [];
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1.length[i] === arr2.length[i]) {
+      const pairs = [arr1[i], arr2[i]];
+      pairArr.push(pairs);
+    }
+  }
+  return pairArr;
+
+}
