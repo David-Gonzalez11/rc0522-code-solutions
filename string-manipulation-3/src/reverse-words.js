5e135dbd50567a9ce45fc12844056ce9094de0bb
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-22 18:09:56 +0000

    Completed string-manipulation-3

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,5 +1,8 @@
 /* exported reverseWords */
+// define a new varibale and set it equal to the str parameter then call the split method and use a space as the methods parameter
+// use the map method to map over all the characters and reverse the characters with the reverse method and then join them back together with no spaces
+// return the join method with a space as its parameter of the reversedWordArr
 function reverseWords(str) {
-  const reverseWordArr = str.split(' ').map(word => word.split('').reverse().join(''));
+  var reverseWordArr = str.split(' ').map(word => word.split('').reverse().join(''));
   return reverseWordArr.join(' ');
 }
