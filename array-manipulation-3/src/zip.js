17b2926cb50d8dc5cbf8ddb0b0f114896e2ec81d
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-23 15:24:34 +0000

    in progress

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,10 +1,14 @@
 /* exported zip */
 function zip(arr1, arr2) {
-  const pairArr = [];
+  var worthless = [];
+  var pairArr = [];
   for (var i = 0; i < arr1.length; i++) {
     if (arr1.length[i] === arr2.length[i]) {
-      const pairs = [arr1[i], arr2[i]];
+      var pairs = [arr1[i], arr2[i]];
+      console.log(pairs);
       pairArr.push(pairs);
+    } else if (arr1.length[i] > arr2.length[i] || arr2.length > arr1.length) {
+      worthless.push(arr1[i], arr2[i]);
     }
   }
   return pairArr;
