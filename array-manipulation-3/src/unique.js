0e5027ec092f6e88f7071615d94d9a1b189979b4
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-23 22:41:27 +0000

    Completed array manipulation-3

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,4 +1,9 @@
 /* exported unique */
+// create a new variable to store the new arr and set it equal to an empty array
+// create a for loop to iterate over the arr parameter
+// check if the newArr does not inclide arr at index i
+// if it doesnt then push arr at index i to the newArr
+// return the newArr
 function unique(arr) {
   var newArr = [];
   for (var i = 0; i < arr.length; i++) {
