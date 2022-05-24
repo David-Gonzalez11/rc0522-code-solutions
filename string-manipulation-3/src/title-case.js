70606bf935a518f813d75adcc58e14c3787f7546
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-24 00:20:44 +0000

    In progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,16 +1,18 @@
 /* exported titleCase */
 function titleCase(title) {
   var words = title.toLowerCase().split(' ');
-  var minorwordss = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+  var minor = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
 
   for (var i = 0; i < words.length; i++) {
     if (words[i].includes('-')) {
       var splitHyphen = words[i].split('-');
       for (var j = 0; j < splitHyphen.length; j++) {
         splitHyphen[j] = splitHyphen[j].charAt(0).toUpperCase() + splitHyphen[j].slice(1);
+        console.log(splitHyphen);
       }
 
       var hyphen = splitHyphen.join('-');
+      console.log(hyphen);
       words[i] = hyphen;
 
     } else if (words[i] === 'api') {
@@ -21,12 +23,17 @@ function titleCase(title) {
       words[i] = 'JavaScript';
     } else if (words[i].length >= 4) {
       words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      console.log(words[i]);
     } else if (i === 0) {
       words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-    } else if (minorwordss.indexOf(words[i]) === -1) {
+      console.log('if i === 0', words[i]);
+    } else if (minor.indexOf(words[i]) === -1) {
       words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      console.log('lol'
+        , words[i]);
     } else if (words[i - 1].includes(':')) {
       words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      console.log(words[i]);
     }
   }
   var output = words.join(' ');
