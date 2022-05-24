5d9cb67eae65d78573df324472da6866b905fd87
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-24 18:02:12 +0000

    Completed javascript-light-bulb

---
+++ javascript-light-bulb/main.js
@@ -0,0 +1,15 @@
+var div = document.querySelector('.firstdiv');
+var body = document.querySelector('#back');
+var isActive = false;
+
+function handleCLick(event) {
+  if (isActive === false) {
+    div.className = 'firstdiv off';
+    body.className = 'back';
+  } else if (isActive === true) {
+    body.className = '';
+    div.className = 'firstdiv';
+  }
+  isActive = !isActive;
+}
+div.addEventListener('click', handleCLick);
