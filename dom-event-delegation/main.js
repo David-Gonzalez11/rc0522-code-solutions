cbf9182e7864d1b809731c5073a3b36fd9934ff9
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-26 18:36:45 +0000

    made changes to only button being clicked will remove item

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,7 +3,7 @@ list.addEventListener('click', handleClick);
 function handleClick(event) {
   console.log('event.target:', event.target);
   console.log('value of event.target.tagName:', event.target.tagName);
-  if (event.target.tagName === 'BUTTON') {
+  if (event.target.tagName !== 'BUTTON') {
     return;
   }
   var taskListItem = event.target.closest('.task-list-item');
