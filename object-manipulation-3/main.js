c545dfa75dc25a8111a9af837e0a480207c7410b
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-05-27 05:10:22 +0000

    In progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,16 +1,16 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 var players = [
-  { name: 'Frodo', hand: [], score: 0 },
-  { name: 'Samwise', hand: [], score: 0 },
-  { name: 'Merry', hand: [], score: 0 },
-  { name: 'Pippin', hand: [], score: 0 }
+  { name: 'David', hand: [], score: 0 },
+  { name: 'Daniel', hand: [], score: 0 },
+  { name: 'Sam', hand: [], score: 0 },
+  { name: 'Isabel', hand: [], score: 0 }
 ];
 
 var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
 var rank = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
 var deck = [];
 
-function createObjects(players) {
+function createDeck(players) {
   var card = {};
   for (var s = 0; s < suit.length; s++) {
     for (var r = 0; r < rank.length; r++) {
@@ -20,7 +20,18 @@ function createObjects(players) {
       card = {};
     }
   }
+  return deck;
 }
-createObjects();
 
-var shuffledDeck = _.shuffle(deck);
+function shuffleDeck(deck) {
+  for (var x = 0; x < deck.length; x++) {
+    var random = Math.random(Math.random() * (x - 1));
+    var temp = deck[random];
+    deck[random] = deck[x];
+    deck[x] = temp;
+  }
+  return deck;
+}
+
+var gameDeck = createDeck();
+console.log(gameDeck);
