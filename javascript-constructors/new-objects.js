023216002c10383244729aa62665ba08ed85ee73
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-06-07 17:40:47 +0000

    MAde adjustments to my newObejct js

--- javascript-constructors/new-objects.js
+++ javascript-constructors/new-objects.js
@@ -1,23 +1,23 @@
 var anObject = new Object();
 console.log('value of anObject', anObject);
-console.log('type of anObject', anObject);
+console.log('type of anObject', typeof anObject);
 
 var aArray = new Array();
 console.log('value of Array', aArray);
-console.log('type of Array', aArray);
+console.log('type of Array', typeof aArray);
 
 var aFunction = new Function();
 console.log('value of Function', aFunction);
-console.log('type of Function', aFunction);
+console.log('type of Function', typeof aFunction);
 
 var aString = new String();
 console.log('value of String', aString);
-console.log('type of String', aString);
+console.log('type of String', typeof aString);
 
 var aNumber = new Number();
 console.log('value of Number', aNumber);
-console.log('type of Number', aNumber);
+console.log('type of Number', typeof aNumber);
 
 var aBoolean = new Boolean();
 console.log('value of Boolean', aBoolean);
-console.log('type of Boolean', aBoolean);
+console.log('type of Boolean', typeof aBoolean);
