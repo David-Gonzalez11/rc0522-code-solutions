3b8124f04b39d1547ed0ad8a54cbb559484d7015
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-07-16 00:29:04 +0000

    implemented feedback and tested, works good

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 class RegistrationForm extends React.Component {
   constructor(props) {
@@ -46,8 +46,7 @@ class RegistrationForm extends React.Component {
   }
 }
 
-const root = document.querySelector('#root');
-ReactDOM.render(
-  <RegistrationForm />,
-  root
-);
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<RegistrationForm />);
