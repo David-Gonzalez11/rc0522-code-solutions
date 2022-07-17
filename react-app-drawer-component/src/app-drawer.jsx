6d904bb37f463dec92b7bbfa5aa41deca6564f49
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-07-17 06:35:04 +0000

    In progress

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggled: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      isToggled: !this.state.isToggled
+    });
+  }
+
+  openDrawer() {
+    if (this.state.isToggled) {
+      return 'drawer open';
+    } else {
+      return 'drawer';
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <i className="fa-solid fa-bars fa-2x" onClick={this.handleClick}></i>
+        <div onClick={this.handleClick} className={this.openDrawer()}>
+          <div className="content">
+            <h1 onClick={this.handleClick}>Text</h1>
+            <a onClick={this.handleClick}>About</a>
+            <a onClick={this.handleClick}>Get Started</a>
+            <a onClick={this.handleClick}>Sign In</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
