19f19469ad22b3ee1b3a6a391e938fa1f5b3f86a
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-07-17 18:27:55 +0000

    In progress i think

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -15,19 +15,13 @@ export default class AppDrawer extends React.Component {
     });
   }
 
-  openDrawer() {
-    if (this.state.isToggled) {
-      return 'drawer open';
-    } else {
-      return 'drawer';
-    }
-  }
-
   render() {
+    const news = this.state.isToggled ? 'drawer open' : 'drawer';
+
     return (
       <div>
         <i className="fa-solid fa-bars fa-2x" onClick={this.handleClick}></i>
-        <div onClick={this.handleClick} className={this.openDrawer()}>
+        <div onClick={this.handleClick} className={news}>
           <div className="content">
             <h1 onClick={this.handleClick}>Text</h1>
             <a onClick={this.handleClick}>About</a>
@@ -38,4 +32,5 @@ export default class AppDrawer extends React.Component {
       </div>
     );
   }
+
 }
