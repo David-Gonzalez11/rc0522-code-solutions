1bf48159daae398e80d84e4929fd38bb7778bcc6
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-07-18 15:06:02 +0000

    in progress

---
+++ react-accordion-component/src/accordian.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Accordian extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemViewed: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const key = event.target.id;
+    if (event.target.tagName === 'H5') {
+      if (this.state.itemViewed === key) {
+        this.setState({
+          itemViewed: null
+        });
+      } else {
+        this.setState({
+          itemViewed: key
+        });
+      }
+
+    }
+
+  }
+
+  render() {
+    const topics = this.props.topics;
+    const listTopics = topics.map(topic =>
+      <li key={topic.topic}>
+        <h5 onClick={this.handleClick} id={topic.topic} className="header">{topic.topic}</h5>
+        <p className={this.state.itemViewed === topic.topic ? 'detail' : 'detail hidden'}>{topic.detail}</p>
+      </li>
+    );
+    return (
+        <ul>
+          {listTopics}
+        </ul>
+    );
+  }
+}
+
+export default Accordian;
