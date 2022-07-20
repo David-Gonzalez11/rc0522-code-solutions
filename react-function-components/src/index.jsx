60894906c451c8fbc01ec46fc3bcc5b917e5957f
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-07-20 04:26:21 +0000

    Changed syntac to newer syntax

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 function CustomButton() {
   return <button>Click me!</button>;
 }
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-const custom = <CustomButton />;
-root.render(custom);
+const container = (document.querySelector('#root'));
+const element = <CustomButton />;
+const root = ReactDOM.createRoot(container);
+root.render(element);
