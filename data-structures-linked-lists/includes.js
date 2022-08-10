370b9aa1742f09580ffc26d14495acbb89726d1c
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-08-10 18:28:33 +0000

    Completed linked_lists

--- data-structures-linked-lists/includes.js
+++ data-structures-linked-lists/includes.js
@@ -1,12 +1,14 @@
 /* exported includes */
 
 function includes(list, value) {
-  while (list) {
+  while (list.next !== null) {
     if (list.data === value) {
       return true;
     }
     list = list.next;
   }
+  if (list.data === value) {
+    return true;
+  }
   return false;
-
 }
