d486e9ad1610019c03ea618e9d91154010b9b413
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-08-24 05:21:49 +0000

    made arrows work on click to show next image

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,67 +3,87 @@ import React from 'react';
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 1
-    };
+    this.state = { currentImage: 0 };
     this.leftClick = this.leftClick.bind(this);
     this.rightClick = this.rightClick.bind(this);
-    this.interval = this.interval.bind(this);
-    this.circles = this.circles.bind(this);
   }
 
-  leftClick(event) {
-    clearInterval(this.timer);
-    this.timer = setInterval(() => this.arrowRight(), 3000);
-    if (this.state.currentView <= 1) {
-      this.setState({ currentView: this.props.images.length });
-    } else {
-      this.setState(previousState => ({
-        currentView: previousState.currentView - 1
-      }));
+  // componentDidMount() {
+  //   this.timer = setInterval(this.rightClick, 3000);
 
+  // }
+
+  leftClick(event) {
+    console.log('I am working');
+
+    const currentImage = this.state.currentImage;
+    console.log('value of currentIMage', currentImage);
+    if (currentImage === 0) {
+      this.setState({ currentImage: 4 });
+    } else {
+      this.setState({ currentImage: currentImage - 1 });
     }
+    // clearInterval(this.timer);
+    // this.timer = setInterval(this.rightClick, 3000);
   }
 
   rightClick() {
+    console.log('I am working');
+
+    const currentImage = this.state.currentImage;
+    console.log('value of currentIMage', currentImage);
+
+    if (currentImage === this.props.images.length - 1) {
+      this.setState({ currentImage: 0 });
+    } else {
+      this.setState({ currentImage: currentImage + 1 });
+    }
+    // clearInterval(this.timer);
+    // this.timer = setInterval(this.rightClick, 3000);
   }
 
-  interval() {
+  // handleCircles(event) {
+  //   const images = this.props.images;
+  //   const currentImage = this.state.currentImage;
+  //   const change = event.target === images[currentImage]
+  //     ? 'fas fa-circle circle-icon fa-2x'
+  //     : 'far fa-circle circle-icon fa-2x';
 
-  }
-
-  circles() {
-
-  }
+  // }
 
   render() {
+    const images = this.props.images;
+    const currentImage = this.state.currentImage;
+
+    const listItems = images.map(number =>
+  <li key={images.id}>
+    <img src={number.url}/>
+    </li>
+    );
     return (
       <>
-<div className="container">
-    <div className="row">
+       <div className="row">
       <div className="arrow-left-icon column">
         <i className="fa-solid fa-angle-left fa-7x" onClick={this.leftClick}></i>
       </div>
       <div className="images column">
-        <img src="https://encrypted-tbn0..com/images?q=tbn:ANd9GcQ6AKNjklQ1jlv8mLgpBmo3wLpEgsYvIqYf0--wmEceLAIQkJMj6sX3QnU_ObXxDPj_E1w&usqp=CAU"/>
+        <img src={images[currentImage].url}/>
       </div>
-
       <div className="arrow-right-icon column">
         <i className="fa-solid fa-chevron-right fa-7x" onClick={this.rightClick}></i>
       </div>
     </div>
-
-  </div>
-  <div className="row column-middle">
+<div className="row column-middle">
     <div className="buttons">
-      {/* <i className="fa-regular fa-circle fa-2x" data-view="0"></i>
+      <i className="fa-regular fa-circle fa-2x" data-view="0" onClick={e => this.handleCircles}></i>
       <i className="fa-regular fa-circle fa-2x" data-view="1"></i>
       <i className="fa-regular fa-circle fa-2x" data-view="2"></i>
       <i className="fa-regular fa-circle fa-2x" data-view="3"></i>
-      <i className="fa-regular fa-circle fa-2x" data-view="4"></i> */}
+      <i className="fa-regular fa-circle fa-2x" data-view="4"></i>
     </div>
-  </div>
-</>
+    </div>
+    </>
+
     );
   }
 }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
+
 const images = [
   {
-    id: 1,
-    image: '../dist/images/025.png'
+    url: '../dist/images/001.png',
+    id: 1
   },
   {
-    id: 2,
-    image: '../dist/images/001.png'
-
+    url: '../dist/images/004.png',
+    id: 2
   },
   {
-    id: 3,
-    image: '../dist/images/004.png'
+    url: '../dist/images/007.png',
+    id: 3
   },
   {
-    id: 4,
-    image: '../dist/images/007.png'
+    url: '../dist/images/025.png',
+    id: 4
   },
   {
-    id: 5,
-    image: '../dist/images/039.png'
+    url: '../dist/images/039.png',
+    id: 5
   }
-
 ];
 const element = (
-<Carousel images={images} />
+  <Carousel images={images} />
+
 );
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
