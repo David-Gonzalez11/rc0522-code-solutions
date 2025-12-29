ce2f35dfbf688f7c5dacc100b2b279121a0e890d
David Gonzalez <davidgonzalez_1999@yahoo.com>
2025-12-29 15:25:31 -0800

    work in progress

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,12 +1,26 @@
-var list = document.querySelector('.task-list');
-list.addEventListener('click', handleClick);
-function handleClick(event) {
-  console.log('event.target:', event.target);
-  console.log('value of event.target.tagName:', event.target.tagName);
-  if (event.target.tagName !== 'BUTTON') {
-    return;
-  }
-  var taskListItem = event.target.closest('.task-list-item');
+// var list = document.querySelector('.task-list');
+// list.addEventListener('click', handleClick);
+// function handleClick(event) {
+//   console.log('event.target:', event.target);
+//   console.log('value of event.target.tagName:', event.target.tagName);
+//   if (event.target.tagName !== 'BUTTON') {
+//     return;
+//   }
+//   var taskListItem = event.target.closest('.task-list-item');
+//   console.log('closest item:', taskListItem);
+//   taskListItem.remove();
+// }
+
+
+var list = document.querySelector(".task-list");
+list.addEventListener("click", handleClick)
+
+function handleClick(event){
+if(event.target.tagName !=="BUTTON"){
+  return;
+} 
+var taskListItem = event.target.closest(".task-list-item")
   console.log('closest item:', taskListItem);
-  taskListItem.remove();
+
+taskListItem.remove();
 }
