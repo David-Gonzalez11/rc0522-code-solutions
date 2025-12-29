5cbaf129da9b1e4bd6194feecc1d16e1505405b3
David Gonzalez <davidgonzalez_1999@yahoo.com>
2025-12-29 11:00:23 -0800

    wokring on light bulb again, made some changes

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,34 @@
-var div = document.querySelector('.firstdiv');
-var body = document.querySelector('#back');
-var isActive = false;
 
-function handleCLick(event) {
-  if (isActive === false) {
-    div.className = 'firstdiv off';
-    body.className = 'back';
-  } else if (isActive === true) {
-    body.className = '';
-    div.className = 'firstdiv';
-  }
-  isActive = !isActive;
+
+
+var div = document.querySelector(".firstdiv");
+var body = document.querySelector("#back")
+var isActive= false;
+ 
+
+function handleCLick(event){
+if(isActive === false){
+  div.className="firstdiv off";
+  body.className="back";
+} else {
+  body.className = ""
+  div.className="firstdiv"
 }
-div.addEventListener('click', handleCLick);
+isActive = !isActive
+}
+div.addEventListener('click', handleCLick)
+// var div = document.querySelector('.firstdiv');
+// var body = document.querySelector('#back');
+// var isActive = false;
+
+// function handleCLick(event) {
+//   if (isActive === false) {
+//     div.className = 'firstdiv off';
+//     body.className = 'back';
+//   } else if (isActive === true) {
+//     body.className = '';
+//     div.className = 'firstdiv';
+//   }
+//   isActive = !isActive;
+// }
+// div.addEventListener('click', handleCLick);
