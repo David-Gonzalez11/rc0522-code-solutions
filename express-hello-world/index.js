0f1b6efb307210f1fec91ffabe7edc082cb4e5f2
David Gonzalez <davidgonzalez_1999@yahoo.com>
2025-12-30 14:19:23 -0800

    re did express-hello world for more practice

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -1,8 +1,23 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+
+// app.use((req, res) => {
+//   res.send('Hello World');
+// });
+// app.listen(3000, () => {
+// });
+
+
+
+
+const express = require("express");
 const app = express();
 
 app.use((req, res) => {
-  res.send('Hello World');
+  res.send("Hello world")
 });
+
 app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+
 });
