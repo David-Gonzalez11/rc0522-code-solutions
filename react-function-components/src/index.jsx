eae8d14378d14885506c504e801c95024d4cd42e
David Gonzalez <davidgonzalez_1999@yahoo.com>
2025-12-31 10:15:58 -0800

    work in progress

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function CustomButton() {
-  return <button>Click me!</button>;
+  return <button> Click me!</button>;
 }
-const container = (document.querySelector('#root'));
+
+const container = document.querySelector('#root');
 const element = <CustomButton />;
 const root = ReactDOM.createRoot(container);
 root.render(element);
