13f05ec8d8a0f2d9c493e994862ae52c934dca18
David Gonzalez <davidgonzalez_1999@yahoo.com>
2025-12-31 13:50:16 -0800

    need more practice

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -8,26 +8,57 @@ class ToggleSwitch extends React.Component {
   }
 
   handleClick() {
-    this.setState(prevstate => ({
-      isToggled: !prevstate.isToggled
+    this.setState(prevState => ({
+      isToggled: !prevState.isToggled
     }));
   }
 
   render() {
     let status = 'off';
-
     if (this.state.isToggled) {
       status = 'on';
     }
-
     return (
-  <div className="row">
-    <div className={status} onClick={this.handleClick}>
-      <button className={`toggle-${status}`}></button>
-    </div>
+      <div className="row">
+     <div className={status} onClick={this.handleClick}>
+       <button className={`toggle-${status}`} ></button>
+     </div>
     <div>{status.toUpperCase()}</div>
-  </div>
+     </div>
     );
   }
 }
+
 export default ToggleSwitch;
+
+// class ToggleSwitch extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isToggled: false };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState(prevstate => ({
+//       isToggled: !prevstate.isToggled
+//     }));
+//   }
+
+//   render() {
+//     let status = 'off';
+
+//     if (this.state.isToggled) {
+//       status = 'on';
+//     }
+
+//     return (
+//   <div className="row">
+//     <div className={status} onClick={this.handleClick}>
+//       <button className={`toggle-${status}`}></button>
+//     </div>
+//     <div>{status.toUpperCase()}</div>
+//   </div>
+//     );
+//   }
+// }
+// export default ToggleSwitch;
