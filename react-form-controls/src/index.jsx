a83fa647e39eb5b94b7bc16f0a9ac3b601741e67
David Gonzalez <davidgonzalez_1999@yahoo.com>
2026-01-02 11:32:57 -0800

    re learning form

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -8,45 +8,45 @@ class RegistrationForm extends React.Component {
       value: '',
       username: '',
       password: ''
-    };
 
+    };
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
   }
 
-  handleUsernameChange(event) {
-    this.setState({ username: event.target.value });
+  handleUsernameChange(e) {
+    this.setState({ username: e.target.value });
   }
 
-  handlePasswordChange(event) {
+  handlePasswordChange(e) {
     this.setState({
-      password: event.target.value
+      password: e.target.value
     });
   }
 
-  handleSubmit(event) {
-    console.log(`state: ${this.state}`);
-    event.preventDefault();
+  handleSubmit(e) {
+    console.log(this.state);
+    e.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Username:
+    <form onSubmit={this.handleSubmit}>
+      <label>Username:
         <input type="text" value={this.state.username} onChange={this.handleUsernameChange}></input>
-        </label>
-        <label>
-          Password:
-          <input type="password" value={this.state.password} onChange={this.handlePasswordChange}></input>
-        </label>
-        <input type="submit" value="Sign up" />
-      </form>
+      </label>
+      <label>Password:
+        <input type="text" value={this.state.password} onChange={this.handlePasswordChange}></input>
+      </label>
+      <input type='submit' value="sign up"></input>
+    </form>
     );
   }
 }
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-
 root.render(<RegistrationForm />);
+
+export default RegistrationForm;
